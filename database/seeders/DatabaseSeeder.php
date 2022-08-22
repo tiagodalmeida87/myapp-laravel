8647976ef2b79eb3effc2f7af03f6e456bc2b1ad
Tiago Almeida <tiagodalmeida87@hotmail.com>
2022-08-21 23:47:11 -0300

    ajuste commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-
         $this->call(ProductSeeder::class);
 
         // \App\Models\User::factory()->create([
